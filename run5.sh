45c1484bc0ee67758b7120a8164244df8cef8209
string-12 <ethan920912@gmail.com>
2024-11-13 01:13:23 +0800

    new gravity

--- run5.sh
+++ run5.sh
@@ -1,4 +1,4 @@
-list=(1.0 2.0 3.0 4.0 5.0 8.0 10.0 20.0 50.0 100.0)
+list=(2.0 3.0 4.0 5.0 7.0 10.0 20.0 50.0)
 spinlist=(0.0)
 dir=$(pwd)
 echo "$dir"
@@ -25,7 +25,7 @@ for ((i = 0; i < ${#list[@]}; i++))
 	echo "Running sdpb"
         docker run --shm-size=4096m -v "$dir":/data bootstrapcollaboration/sdpb:master mpirun --allow-run-as-root -n 48 sdpb --precision=1024 --procsPerNode=32 --maxIterations=5000 -s /data/out
         
-	python3 store_result.py out_out/out.txt result/spin0.txt
+	python3 store_result.py out_out/out.txt result/spin0_fast.txt
 
         #rm -rf upper_spectrum 
 
