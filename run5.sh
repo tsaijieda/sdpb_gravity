7f6bf02368acf9941f4b7dcc9ac5a0aec2f6208f
string-12 <ethan920912@gmail.com>
2025-02-03 14:42:23 +0800

    run5

--- run5.sh
+++ run5.sh
@@ -1,4 +1,5 @@
-list=(1.0 1.1 1.2 1.3 1.4 1.5 1.7 1.9 2.0 3.0 4.0 5.0 7.0 10.0 20.0 50.0)
+list=(-2.0 0.0 2.0 4.0 6.0 8.0 10.0 15.0 20.0)
+#list=(2.0 1.0 1.1 1.2 1.3 1.5 1.7 2.0 3.0 4.0 5.0 7.0 10.0)
 spinlist=(0.0)
 dir=$(pwd)
 echo "$dir"
@@ -17,7 +18,7 @@ for ((i = 0; i < ${#list[@]}; i++))
         python3 change.py range.txt $mu -1 $spin0
 
         echo "Generating xml file"
-        wolframscript -file gravity.m
+        wolframscript -file "gravity-5.m"
 
         echo "Turning xml to sdp file"
         docker run --shm-size=4096m -v "$dir":/data bootstrapcollaboration/sdpb:master pmp2sdp 1024 -i /data/out.json -o /data/out
@@ -25,7 +26,7 @@ for ((i = 0; i < ${#list[@]}; i++))
 	echo "Running sdpb"
         docker run --shm-size=4096m -v "$dir":/data bootstrapcollaboration/sdpb:master mpirun --allow-run-as-root -n 48 sdpb --dualityGapThreshold=1e-30 --primalErrorThreshold=1e-30 --dualErrorThreshold=1e-30 --precision=1024 --procsPerNode=32 --maxIterations=5000 -s /data/out
         
-	python3 store_result.py out_out/out.txt result/spin2_change_g.txt
+	python3 store_result.py out_out/out.txt result/gravity_new.txt
 
         #rm -rf upper_spectrum 
 
